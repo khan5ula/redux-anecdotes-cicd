140678f4a66d187a48ab9117120e183f94761fe5
Kristian Hannula <kristian.hannula@proton.me>
2023-09-06 14:24:16 +0300

    backend serves frontend prod build now

--- frontend/src/services/anecdotes.js
+++ frontend/src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/anecdotes'
+const baseUrl = '/api/anecdotes'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
